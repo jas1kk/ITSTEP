88c36c7266ac612d4f139b2097449bcdd158a9f2
jas1kk <109805550+jas1kk@users.noreply.github.com>
2022-08-22 21:40:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// function myFunc() {
+//     let num = 10;
+//     num++;
+//     console.log(num);
+
+//     return num;
+// }
+
+// let myVar = myFunc();
+// console.log();
+// let userNumber = prompt(813);
+// let userNumber2 = prompt();
+// userNumber = userNumber / userNumber2   
+// function speedConverter(valNum) {
+//     document.getElementById("outputMPH").innerHTML=valNum/1.609344;
+// }
+
+// let a = 463;
+// let last = a % 10
+
+// a = a - last
+// a = a / 10
+
+// a = a % 10
+
+// console.log(a); 463
+
+// var n = 12345 + '';
+// var rez = n[n.length -1];
+// for(var i = n.length - 2; i >= 0; i--)
+//     rez += n[i];
+// alert(rez);
+
+
+
+// let a = 97341
+
+
+// let last = a % 10
+// a = ((last * 100000 + a) - last) / 1
+
+
+// console.log(a); 197341
+
+// let userNumber = prompt();
